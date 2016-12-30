0bc08a4f44ae47185f0ee0ee79ade65355c39195
Anythingtechpro@gmail.com <anythingtechpro@gmail.com>
2016-12-30 17:11:51 -0500

    initial code commit

---
+++ win32/start_game.bat
@@ -0,0 +1,24 @@
+@echo off
+cd ..
+
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+rem Get the user input:
+set /P ttiUsername="Username: "
+set /P TTI_GAMESERVER="Gameserver (DEFAULT: 167.114.28.238): " || ^
+set TTI_GAMESERVER=167.114.28.238
+
+rem Export the environment variables:
+set ttiPassword=password
+set TTI_PLAYCOOKIE=%ttiUsername%
+
+echo ===============================
+echo Starting Toontown Infinite...
+echo ppython: %PPYTHON_PATH%
+echo Username: %ttiUsername%
+echo Gameserver: %TTI_GAMESERVER%
+echo ===============================
+
+%PPYTHON_PATH% -m toontown.toonbase.ClientStart
+pause
