4149deab0538c63f6ba2713d219c0ab4e5362843
Anythingtechpro@gmail.com <anythingtechpro@gmail.com>
2017-01-03 16:33:14 -0500

    Updated  for Nuitka support

--- toontown/toonbase/ClientStartDist.py
+++ toontown/toonbase/ClientStartDist.py
@@ -7,30 +7,7 @@ sys.path = ['.']
 import collections
 collections.namedtuple = lambda *x: tuple
 
-# This is included in the package by the prepare_client script. It contains the
-# PRC file data, and (stripped) DC file:
-import game_data
-import __builtin__
-
-# Load all of the packaged PRC config page(s):
-from pandac.PandaModules import *
-for i, config in enumerate(game_data.CONFIG):
-    name = 'GameData config page #' + str(i)
-    loadPrcFileData(name, config)
-
-# The VirtualFileSystem, which has already initialized, doesn't see the mount
-# directives in the config(s) yet. We have to force it to load them manually:
-vfs = VirtualFileSystem.getGlobalPtr()
-mounts = ConfigVariableList('vfs-mount')
-for mount in mounts:
-    mountFile, mountPoint = (mount.split(' ', 2) + [None, None, None])[:2]
-    mountFile = Filename(mountFile)
-    mountFile.makeAbsolute()
-    mountPoint = Filename(mountPoint)
-    vfs.mount(mountFile, mountPoint, 0)
-
-# Next, let's get the DC stream:
-__builtin__.dcStream = StringStream(game_data.DC)
+# TODO: append resources, and load config from stream string.
 
 # Finally, start the game:
 import toontown.toonbase.ClientStart
