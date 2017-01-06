55b92606326274581b9afab39587a858620cd61c
BarksAlot <noahspoons1@gmail.com>
2017-01-05 20:22:46 -0500

    Song and Dance head fix (ty Josh)

--- toontown/suit/Suit.py
+++ toontown/suit/Suit.py
@@ -564,13 +564,16 @@ class Suit(Avatar.Avatar):
         modelRoot.find('**/hands').setTexture(handTex, 1)
 
     def generateHead(self, headType):
-        filePrefix, phase = ModelDict[self.style.body]
-        filepath = 'phase_' + str(phase) + filePrefix + 'heads'
-        headModel = NodePath('cog_head')
-        Preloaded[filepath].copyTo(headModel)
+        if config.GetBool('want-new-cogs', 0):
+            filePrefix, phase = HeadModelDict[self.style.body]
+        else:
+            filePrefix, phase = ModelDict[self.style.body]
+        headModel = loader.loadModel('phase_' + str(phase) + filePrefix + 'heads')
         headReferences = headModel.findAllMatches('**/' + headType)
         for i in xrange(0, headReferences.getNumPaths()):
-            headPart = self.instance(headReferences.getPath(i), 'modelRoot', 'joint_head')
+            headPart = self.instance(headReferences.getPath(i), 'modelRoot', 'to_head')
+            if not headPart:
+                headPart = self.instance(headReferences.getPath(i), 'modelRoot', 'joint_head')
             if self.headTexture:
                 headTex = loader.loadTexture('phase_' + str(phase) + '/maps/' + self.headTexture)
                 headTex.setMinfilter(Texture.FTLinearMipmapLinear)
@@ -578,8 +581,8 @@ class Suit(Avatar.Avatar):
                 headPart.setTexture(headTex, 1)
             if self.headColor:
                 headPart.setColor(self.headColor)
-            headPart.flattenStrong()
             self.headParts.append(headPart)
+
         headModel.removeNode()
 
     def generateCorporateTie(self, modelPath = None):
