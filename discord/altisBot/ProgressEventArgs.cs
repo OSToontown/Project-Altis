aa81222f8c950ccdac42efdc4fd33b959ee3c601
Drew <drewgamesaw@gmail.com>
2017-01-08 20:15:34 -0500

    i made an new bot

---
+++ discord/altisBot/ProgressEventArgs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discordCSBOT
+{
+    /// <summary>
+    /// The event arguement class for passing when progress event is fired
+    /// </summary>
+    public class ProgressEventArgs : EventArgs
+    {
+        public object ProcessObject { get; set; }
+        public decimal Percentage { get; set; }
+        public string Error { get; set; }
+
+        public ProgressEventArgs() :
+            base()
+        {
+
+        }
+
+    }
+
+    /// <summary>
+    /// The event arguement class for passing when progress event is fired
+    /// </summary>
+    public class DownloadEventArgs : EventArgs
+    {
+        public object ProcessObject { get; set; }
+        public DownloadEventArgs() :
+            base()
+        {
+
+        }
+
+    }
+}
