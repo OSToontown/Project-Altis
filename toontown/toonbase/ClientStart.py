b10fe52e884f7f7b6c56cb468726adccf64d33ca
ponyboy837 <ponyboy837@gmail.com>
2017-01-11 17:14:28 -0500

    Tested some things

--- toontown/toonbase/ClientStart.py
+++ toontown/toonbase/ClientStart.py
@@ -117,6 +117,36 @@ DirectGuiGlobals.setDefaultDialogGeom(loader.loadModel('phase_3/models/gui/dialo
 from toontown.toonbase import TTLocalizer
 from otp.otpbase import OTPGlobals
 OTPGlobals.setDefaultProductPrefix(TTLocalizer.ProductPrefix)
+
+
+#For Devs only. (The below)
+'''from direct.stdpy import threading, thread
+def __inject_wx(_):
+    code = textbox.GetValue()
+    exec (code, globals())
+
+def openInjector_wx():
+    import wx
+    
+    app = wx.App(redirect = False)
+        
+    frame = wx.Frame(None, title = "TTN Injector", size=(640, 400), style=wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX | wx.MINIMIZE_BOX)
+    panel = wx.Panel(frame)
+    button = wx.Button(parent = panel, id = -1, label = "Inject", size = (50, 20), pos = (295, 0))
+    global textbox
+    textbox = wx.TextCtrl(parent = panel, id = -1, pos = (20, 22), size = (600, 340), style = wx.TE_MULTILINE)
+    frame.Bind(wx.EVT_BUTTON, __inject_wx, button)
+
+    frame.Show()
+    app.SetTopWindow(frame)
+    
+    textbox.AppendText(" ")
+    
+    threading.Thread(target = app.MainLoop).start()
+
+openInjector_wx()'''
+
+
 if base.musicManagerIsValid:
     music = base.loader.loadMusic('phase_3/audio/bgm/tt_theme.ogg')
     if music:
