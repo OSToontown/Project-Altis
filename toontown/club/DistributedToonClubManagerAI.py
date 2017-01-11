da2b1a13404abb42dc0cc61d4ac8085ba213d789
Anythingtechpro@gmail.com <anythingtechpro@gmail.com>
2017-01-10 22:34:30 -0500

    Make the club mgr asynchronous

--- toontown/club/DistributedToonClubManagerAI.py
+++ toontown/club/DistributedToonClubManagerAI.py
@@ -3,12 +3,28 @@ from direct.directnotify import DirectNotifyGlobal
 from direct.fsm.FSM import FSM
 import anydbm
 
+class ClubQueryFSM(FSM):
+
+    def __init__(self, clubManager):
+        self.clubManager = clubManager
+
+    def enterQuertyClub(self, clubId):
+        self.air.dbInterface.queryObject(self.air.dbId, clubId, callback=self.__queryClubCallback)
+
+    def exitQueryClub(self):
+        pass
+
+    def __queryClubCallback(dclass, fields):
+        pass
+
 class DistributedToonClubManagerAI(DistributedObjectGlobalAI, FSM):
     notify = DirectNotifyGlobal.directNotify.newCategory('DistributedToonClubManagerAI')
     
     def __init__(self, cr):
         DistributedObjectGlobalAI.__init__(self, cr)
         FSM.__init__(self, 'DistributedToonClubManagerAI')
+
+        self.avatar2fsm = {}
 
     def generate(self):
         DistributedObjectGlobalAI.generate(self)
@@ -31,14 +47,8 @@ class DistributedToonClubManagerAI(DistributedObjectGlobalAI, FSM):
             self.d_requestStatusResponse(avId, False, 0)
             return
 
-        # query the club database file for all details
-        self.request('QuertClub', clubId)
-
-    def enterQuertyClub(self, clubId):
-        pass
-
-    def exitQueryClub(self):
-        pass
+        self.avatar2fsm[avId] = ClubQueryFSM(self)
+        self.avatar2fsm[avId].request('QueryClub', clubId)
 
     def d_requestStatusResponse(self, avId, clubFound, clubDoId):
         self.sendUpdateToAvatarId(avId, 'requestStatusResponse', [clubFound, clubDoId])
