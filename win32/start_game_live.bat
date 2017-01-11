80d680d0921071661928182930b8672fd9269ca3
Anythingtechpro@gmail.com <anythingtechpro@gmail.com>
2017-01-11 12:20:05 -0500

    Added batch file for live server

---
+++ win32/start_game_live.bat
@@ -0,0 +1,27 @@
+@echo off
+cd ../
+
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+rem Get the user input:
+set /P ttaUsername="Username: "
+
+rem Export the environment variables:
+set ttaPassword=password
+set TTA_PLAYCOOKIE=%ttaUsername%
+set TTA_GAMESERVER=gs1.projectaltis.com
+
+echo ===============================
+echo Starting Toontown Project Altis...
+echo ppython: %PPYTHON_PATH%
+echo Username: %ttaUsername%
+echo Gameserver: %TTA_GAMESERVER%
+echo ===============================
+
+:goto
+
+%PPYTHON_PATH% -m toontown.toonbase.ClientStart
+pause
+
+goto :goto
