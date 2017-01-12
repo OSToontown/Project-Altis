debb00f0ad96c04a6c52f087427637f336a3b9b2
BarksAlot <noahspoons1@gmail.com>
2017-01-12 15:16:57 -0500

    Removed Dance Unites, for it is a TTI feature

--- toontown/chat/ResistanceChat.py
+++ toontown/chat/ResistanceChat.py
@@ -19,7 +19,6 @@ EFFECT_RADIUS = 30
 RESISTANCE_TOONUP = 0
 RESISTANCE_RESTOCK = 1
 RESISTANCE_MONEY = 2
-RESISTANCE_DANCE = 3
 allowedResistanceMessages = []
 if config.GetBool('want-resistance-toonup', True):
     allowedResistanceMessages.append(RESISTANCE_TOONUP)
@@ -27,11 +26,8 @@ if config.GetBool('want-resistance-restock', True):
     allowedResistanceMessages.append(RESISTANCE_RESTOCK)
 if config.GetBool('want-resistance-money', True):
     allowedResistanceMessages.append(RESISTANCE_MONEY)
-if config.GetBool('want-resistance-dance', True):
-    allowedResistanceMessages.append(RESISTANCE_DANCE)
 resistanceMenu = [
-    RESISTANCE_TOONUP, RESISTANCE_RESTOCK, RESISTANCE_MONEY,
-    RESISTANCE_DANCE
+    RESISTANCE_TOONUP, RESISTANCE_RESTOCK, RESISTANCE_MONEY
 ]
 resistanceDict = {
     RESISTANCE_TOONUP: {
@@ -75,13 +71,6 @@ resistanceDict = {
             TTLocalizer.MovieNPCSOSAll
         ],
         'items': [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    },
-    RESISTANCE_DANCE: {
-        'menuName': TTLocalizer.ResistanceDanceMenu,
-        'itemText': TTLocalizer.ResistanceDanceItem,
-        'chatText': TTLocalizer.ResistanceDanceChat,
-        'values': ['Dance'],
-        'items': [0]
     }
 }
 
@@ -199,13 +188,6 @@ def doEffect(textId, speakingToon, nearbyToons):
             p = effect.getParticlesNamed(name)
             p.renderer.setFromNode(icon)
         fadeColor = VBase4(0, 0, 1, 1)
-    elif menuIndex == RESISTANCE_DANCE:
-        effect = BattleParticles.loadParticleFile('resistanceEffectSparkle.ptf')
-        fadeColor = VBase4(1, 0.5, 1, 1)
-        for toonId in nearbyToons:
-            toon = base.cr.doId2do.get(toonId)
-            if toon and (not toon.ghostMode):
-                toon.setAnimState('victory')
     else:
         return
     recolorToons = Parallel()
