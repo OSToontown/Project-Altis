315323fe79bbbfd045f6c9f9ab22127f040adcef
Drew <drewgamesaw@gmail.com>
2017-01-17 18:59:30 -0500

    disable parties gates still show good

--- toontown/toon/DistributedToon.py
+++ toontown/toon/DistributedToon.py
@@ -2658,25 +2658,31 @@ class DistributedToon(DistributedPlayer.DistributedPlayer, Toon.Toon, Distribute
     def setGMIcon(self, gmType=None):
         if hasattr(self, 'gmIcon') and self.gmIcon:
             return
-        
         if not gmType:
             gmType = self._gmType
-        
-        icons = loader.loadModel('phase_3/models/props/gm_icons.bam')
-        searchString = '**/access_level_' + str(gmType)
-        self.gmIcon = icons.find(searchString)
+        iconInfo = [
+            (None, None),
+            ('phase_3.5/models/gui/tt_m_gui_gm_toontroop_getConnected', '**/whistleIcon*'),
+            ('phase_3.5/models/gui/tt_m_gui_gm_toonResistance_fist', '**/*fistIcon*'),
+            ('phase_3.5/models/gui/tt_m_gui_gm_toontroop_getConnected', '**/whistleIcon*'),
+            ('phase_3.5/models/gui/tt_m_gui_gm_toontroop_whistle', '**/whistleIcon*'),
+            ('phase_3.5/models/gui/tt_m_gui_gm_toontroop_whistle', '**/whistleIcon*'),
+            ('phase_3.5/models/gui/tt_m_gui_gm_toontroop_whistle', '**/whistleIcon*')
+        ]
+        index = (gmType / 100) - 1
+        icon = loader.loadModel(iconInfo[index][0])
+        self.gmIcon = icon.find(iconInfo[index][1])
         np = NodePath(self.nametag.getIcon())
         if np.isEmpty():
             return
-        
         self.gmIcon.flattenStrong()
         self.gmIcon.reparentTo(np)
-        self.gmIcon.setScale(1.6)
-        self.gmIcon.setZ(2.05)
+        self.gmIcon.setScale(4)
+        self.gmIcon.setZ(-2.4)
         self.setTrophyScore(self.trophyScore)
         self.gmIconInterval = LerpHprInterval(self.gmIcon, 3.0, Point3(0, 0, 0), Point3(-360, 0, 0))
         self.gmIconInterval.loop()
-
+        
     def setGMPartyIcon(self):
         gmType = self._gmType
         iconInfo = ('phase_3.5/models/gui/tt_m_gui_gm_toonResistance_fist', 'phase_3.5/models/gui/tt_m_gui_gm_toontroop_whistle', 'phase_3.5/models/gui/tt_m_gui_gm_toonResistance_fist', 'phase_3.5/models/gui/tt_m_gui_gm_toontroop_getConnected')
--- toontown/toon/DistributedToonAI.py
+++ toontown/toon/DistributedToonAI.py
@@ -4790,7 +4790,7 @@ def gmIcon(accessLevel=None):
     invoker = spellbook.getInvoker()
     target = spellbook.getTarget()
     invokerAccess = spellbook.getInvokerAccess()
-    if not invokerAccess >= CATEGORY_ADMINISTRATOR.defaultAccess:
+    if invokerAccess < CATEGORY_PROGRAMMER.defaultAccess:
         if accessLevel is not None:
             return "You must be of a higher access level to override your GM icon."
         target = spellbook.getInvoker()
