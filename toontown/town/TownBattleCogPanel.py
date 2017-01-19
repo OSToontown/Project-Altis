8e7358fab9c8fcab5bccefab1fcc32b8b81b1f11
Drew <drewgamesaw@gmail.com>
2017-01-19 18:24:54 -0500

    xxxxxxxxxxxxxxxxxx

--- toontown/town/TownBattleCogPanel.py
+++ toontown/town/TownBattleCogPanel.py
@@ -15,16 +15,26 @@ from toontown.toonbase import TTLocalizer
 
 class TownBattleCogPanel(DirectFrame):
     notify = DirectNotifyGlobal.directNotify.newCategory('TownBattleCogPanel')
-    healthColors = (Vec4(0, 1, 0, 1),
-     Vec4(1, 1, 0, 1),
-     Vec4(1, 0.5, 0, 1),
-     Vec4(1, 0, 0, 1),
-     Vec4(0.3, 0.3, 0.3, 1))
-    healthGlowColors = (Vec4(0.25, 1, 0.25, 0.5),
-     Vec4(1, 1, 0.25, 0.5),
-     Vec4(1, 0.5, 0.25, 0.5),
-     Vec4(1, 0.25, 0.25, 0.5),
-     Vec4(0.3, 0.3, 0.3, 0))
+    healthColors = (Vec4(0, 1, 0, 1),# Green
+     Vec4(0.5, 1, 0, 1),# Green-Yellow
+     Vec4(0.75, 1, 0, 1),# Yellow-Green
+     Vec4(1, 1, 0, 1),# Yellow
+     Vec4(1, 0.866, 0, 1),# Yellow-Orange
+     Vec4(1, 0.6, 0, 1),# Orange-Yellow
+     Vec4(1, 0.5, 0, 1),# Orange
+     Vec4(1, 0.25, 0, 1.0),# Red-Orange
+     Vec4(1, 0, 0, 1), # Red
+     Vec4(0.3, 0.3, 0.3, 1))#9 Gray
+    healthGlowColors = (Vec4(0.25, 1, 0.25, 0.5),#Green
+     Vec4(0.5, 1, 0.25, .5),# Green-Yellow
+     Vec4(0.75, 1, 0.25, .5),# Yellow-Green
+     Vec4(1, 1, 0.25, 0.5),# Yellow 
+     Vec4(1, 0.866, 0.25, .5),# Yellow-Orange
+     Vec4(1, 0.6, 0.25, .5),# Orange-Yellow
+     Vec4(1, 0.5, 0.25, 0.5),# Orange
+     Vec4(1, 0.25, 0.25, 0.5),# Red-Orange    
+     Vec4(1, 0.25, 0.25, 0.5),# Red     
+     Vec4(0.3, 0.3, 0.3, 0))# Gray
 
     def __init__(self, id):
         if settings['newGui'] == True:
