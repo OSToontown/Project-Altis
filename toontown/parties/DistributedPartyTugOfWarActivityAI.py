4bacca1a4d1fb8f1f2b84bd1ff93f0cf58c13cca
ponyboy837 <ponyboy837@gmail.com>
2017-01-21 17:56:10 -0500

    Party Fixes

--- toontown/parties/DistributedPartyTugOfWarActivityAI.py
+++ toontown/parties/DistributedPartyTugOfWarActivityAI.py
@@ -1,8 +1,25 @@
 from direct.directnotify import DirectNotifyGlobal
 from toontown.parties.DistributedPartyTeamActivityAI import DistributedPartyTeamActivityAI
+from toontown.parties import PartyGlobals
+
+scoreRef = {'cat': (PartyGlobals.TugOfWarTieReward, PartyGlobals.TugOfWarTieReward),
+            0: (PartyGlobals.TugOfWarWinReward, PartyGlobals.TugOfWarLossReward),
+            1: (PartyGlobals.TugOfWarLossReward, PartyGlobals.TugOfWarWinReward),
+            2:(PartyGlobals.TugOfWarFallInWinReward, PartyGlobals.TugOfWarFallInLossReward),
+            3: (PartyGlobals.TugOfWarFallInLossReward, PartyGlobals.TugOfWarFallInWinReward),
+           }
 
 class DistributedPartyTugOfWarActivityAI(DistributedPartyTeamActivityAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedPartyTugOfWarActivityAI")
+    
+    def __init__(self, air, parent, activityTuple):
+        DistributedPartyTeamActivityAI.__init__(self, air, parent, activityTuple)
+        self.forces = {}
+        self.pos = 0
+        self._hasFall = 0
+    
+    def getDuration(self):
+        return PartyGlobals.TugOfWarDuration
 
     def reportKeyRateForce(self, todo0, todo1):
         pass
@@ -10,12 +27,9 @@ class DistributedPartyTugOfWarActivityAI(DistributedPartyTeamActivityAI):
     def reportFallIn(self, todo0):
         pass
 
-    def setToonsPlaying(self, todo0, todo1):
-        pass
-
     def updateToonKeyRate(self, todo0, todo1):
         pass
 
-    def updateToonPositions(self, todo0):
+    def d_updateToonPositions(self):
         pass
 
