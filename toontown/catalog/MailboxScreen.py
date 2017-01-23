86b20931ffc482acde9d2116c39c24137cad2333
Drew <drewgamesaw@gmail.com>
2017-01-23 17:53:51 -0500

    kys

--- toontown/catalog/MailboxScreen.py
+++ toontown/catalog/MailboxScreen.py
@@ -11,7 +11,7 @@ from toontown.toontowngui import TTDialog
 from toontown.toontowngui.TeaserPanel import TeaserPanel
 from toontown.parties.InviteVisual import InviteVisual
 import CatalogItem
-from direct.showbase.PythonUtil import StackTrace
+from toontown.toonbase.ToonPythonUtil import StackTrace
 
 class MailboxScreen(DirectObject.DirectObject):
     notify = directNotify.newCategory('MailboxScreen')
