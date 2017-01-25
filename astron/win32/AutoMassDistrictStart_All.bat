714bc889e1e6f05918d855152f0c065656084d64
meme <meme>
2017-01-25 02:38:32 +0100

    remove some districts

--- astron/win32/AutoMassDistrictStart_All.bat
+++ astron/win32/AutoMassDistrictStart_All.bat
@@ -1,8 +1,6 @@
 start DONT_LAUNCH_massDistrictStart_District.bat "Sillyham" 401000000
 start DONT_LAUNCH_massDistrictStart_District.bat "Nutty River" 402000000
 start DONT_LAUNCH_massDistrictStart_District.bat "Barking Boulevard" 403000000
-start DONT_LAUNCH_massDistrictStart_District.bat "Geezer Gorge" 404000000
-start DONT_LAUNCH_massDistrictStart_District.bat "Nuttyboro" 405000000
-start DONT_LAUNCH_massDistrictStart_District.bat "Gigglyham" 406000000
-start DONT_LAUNCH_massDistrictStart_District.bat "Boingyboro" 407000000
-start DONT_LAUNCH_massDistrictStart_District.bat "Crazy Hills" 408000000
+start DONT_LAUNCH_massDistrictStart_District.bat "Nuttyboro" 404000000
+start DONT_LAUNCH_massDistrictStart_District.bat "Gigglyham" 405000000
+start DONT_LAUNCH_massDistrictStart_District.bat "Crazy Hills" 406000000
