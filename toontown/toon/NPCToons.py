7d13db30986d8bfdfa1b2259bf68a75c43e16aeb
Drew <drewgamesaw@gmail.com>
2017-01-29 16:32:43 -0500

    loopy (pls test)

--- toontown/toon/NPCToons.py
+++ toontown/toon/NPCToons.py
@@ -67,6 +67,7 @@ NPC_PARTYPERSON = 8
 NPC_SPECIALQUESTGIVER = 9
 NPC_FLIPPYTOONHALL = 10
 NPC_SCIENTIST = 11
+NPC_LOOPY = 12
 CLERK_COUNTDOWN_TIME = 120
 TAILOR_COUNTDOWN_TIME = 300
 RTDNAFile = '/RTDNAFile.txt'
@@ -90,6 +91,8 @@ def createNPC(air, npcId, desc, zoneId, posIndex = 0, questCallback = None):
     from toontown.toon import DistributedNPCSpecialQuestGiverAI
     from toontown.toon import DistributedNPCFlippyInToonHallAI
     from toontown.toon import DistributedNPCScientistAI
+    from toontown.toon import DistributedNPCLoopyGAI
+    
     canonicalZoneId, name, dnaType, gender, protected, type = desc
     if type == NPC_REGULAR:
         npc = DistributedNPCToonAI.DistributedNPCToonAI(air, npcId, questCallback=questCallback)
@@ -115,6 +118,8 @@ def createNPC(air, npcId, desc, zoneId, posIndex = 0, questCallback = None):
         npc = DistributedNPCFlippyInToonHallAI.DistributedNPCFlippyInToonHallAI(air, npcId)
     elif type == NPC_SCIENTIST:
         npc = DistributedNPCScientistAI.DistributedNPCScientistAI(air, npcId)
+    elif type == NPC_LOOPY:
+        npc =  DistributedNPCLoopyGAI.DistributedNPCLoopyGAI(air, npcId)
     else:
         print 'createNPC() error!!!'
     npc.setName(name)
@@ -2112,7 +2117,7 @@ NPCToonDict = {20000: (-1,
 		 13),
 		'm',
 		0,
-		NPC_REGULAR),
+		NPC_LOOPY),
  1001: (1506,
         lnames[1001],
         ('rss',
