7a272ff50b27521930b530d13d73a2c6744c81ea
Drew <drewgamesaw@gmail.com>
2017-02-02 22:51:55 -0500

    kys rainerino

--- toontown/suit/DistributedSellbotBoss.py
+++ toontown/suit/DistributedSellbotBoss.py
@@ -48,7 +48,6 @@ class DistributedSellbotBoss(DistributedBossCog.DistributedBossCog, FSM.FSM):
         self.dooberRequest = None
         self.bossDamage = 0
         self.attackCode = None
-        self.rain = None
         self.attackAvId = 0
         self.recoverRate = 0
         self.recoverStartTime = 0
@@ -70,24 +69,6 @@ class DistributedSellbotBoss(DistributedBossCog.DistributedBossCog, FSM.FSM):
         self.localToonPromoted = True
         self.resetMaxDamage()
 
-    def startRain(self):
-        if not settings.get('want-particle-effects', True):
-            pass
-        else:
-            self.rain = BattleParticles.loadParticleFile('raindisk.ptf')
-            self.rain.setPos(0, 0, 20)
-            self.rainRender = render.attachNewNode('rainRender')
-            self.rainRender.setDepthWrite(0)
-            self.rainRender.setBin('fixed', 1)
-            self.rain.start(camera, self.rainRender)
-            self.rainSound = base.loader.loadSfx('phase_12/audio/sfx/CHQ_rain_ambient.ogg')
-            base.playSfx(self.rainSound, looping=1, volume=0.25)
-
-    def stopRain(self):
-        if self.rain:
-            self.rain.cleanup()
-            self.rainSound.stop()
- 
     def announceGenerate(self):
         global OneBossCog
         DistributedBossCog.DistributedBossCog.announceGenerate(self)
@@ -168,9 +149,6 @@ class DistributedSellbotBoss(DistributedBossCog.DistributedBossCog, FSM.FSM):
             toonMopath.finish()
             toonMopath.destroy()
             self.toonMopathInterval.remove(toonMopath)
-            
-        if self.rain:
-           self.stopRain()
 
         if OneBossCog == self:
             OneBossCog = None
@@ -373,7 +351,6 @@ class DistributedSellbotBoss(DistributedBossCog.DistributedBossCog, FSM.FSM):
                 ActorInterval(self, 'turn2Fb'))),
             (34, Sequence(
                 Func(self.clearChat),
-                Func(self.startRain),
                 self.loseCogSuits(self.toonsA, self.battleANode, (0, 18, 5, -180, 0, 0)),
                 self.loseCogSuits(self.toonsB, self.battleBNode, (0, 18, 5, -180, 0, 0)))),
             (37, Sequence(
