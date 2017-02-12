266cf80d86fa886d77f7cefafc668475af238be3
~Dan~ <ponyboy837@users.noreply.github.com>
2017-02-12 10:36:00 -0500

    Remove Useless Pass

--- toontown/suit/DistributedBossbotBoss.py
+++ toontown/suit/DistributedBossbotBoss.py
@@ -6,7 +6,7 @@ from direct.fsm import FSM
 from direct.interval.IntervalGlobal import Sequence, Wait, Func, LerpHprInterval, Parallel, LerpPosInterval, Track, ActorInterval, ParallelEndTogether, LerpFunctionInterval, LerpScaleInterval, LerpPosHprInterval, SoundInterval
 from toontown.toonbase import ToonPythonUtil as PythonUtil
 from direct.task import Task
-from pandac.PandaModules import VBase3, CollisionPlane, CollisionNode, CollisionSphere, CollisionTube, NodePath, Plane, Vec3, Vec2, Point3, BitMask32, CollisionHandlerEvent, TextureStage, VBase4, BoundingSphere
+from panda3d.core import VBase3, CollisionPlane, CollisionNode, CollisionSphere, CollisionTube, NodePath, Plane, Vec3, Vec2, Point3, BitMask32, CollisionHandlerEvent, TextureStage, VBase4, BoundingSphere
 from toontown.battle import MovieToonVictory
 from toontown.battle import RewardPanel
 from toontown.battle import SuitBattleGlobals
@@ -963,7 +963,6 @@ class DistributedBossbotBoss(DistributedBossCog.DistributedBossCog, FSM.FSM):
         self.moveTrack = moveTrack
         self.moveTrack.start()
         self.storeInterval(self.moveTrack, 'moveTrack')
-        pass
 
     def interruptMove(self):
         if self.moveTrack and self.moveTrack.isPlaying():
