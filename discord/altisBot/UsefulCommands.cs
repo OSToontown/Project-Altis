76107af4cdb492513d905ce97456a6ebe318a8d9
Drew <drewgamesaw@gmail.com>
2017-02-19 15:05:44 -0500

    ban

--- discord/altisBot/UsefulCommands.cs
+++ discord/altisBot/UsefulCommands.cs
@@ -14,6 +14,7 @@ namespace discordCSBOT
     {
         public void Start(DiscordClient bot)
         {
+
             bot.GetService<CommandService>().CreateCommand("2fa")
                 .Description("Help on how to use 2 factor authentication")
                 .Do(async msg =>
@@ -23,15 +24,29 @@ namespace discordCSBOT
 
             bot.GetService<CommandService>().CreateCommand("ban")
                 .Parameter("user", ParameterType.Required)
-                .Parameter("reason", ParameterType.Multiple)
+                .Parameter("reason", ParameterType.Unparsed)
                 .Do(async msg =>
                 {
-                    IEnumerable<Role> requiredRoles = bot.GetServer(261646233913917443).FindRoles("Management Team", true);
-                    if (msg.Message.User.HasRole(requiredRoles.First<Role>())){
-                        await msg.Message.MentionedUsers.First().SendMessage($"{msg.Message.User.Name} banned {msg.Message.MentionedUsers.First().Name} for reason ({msg.GetArg("reason")})");
-                        await bot.GetServer(261646233913917443).Ban(msg.Message.MentionedUsers.First());
-                        await bot.GetServer(261646233913917443).GetChannel(267022933547941890).SendMessage($"{msg.Message.User.Name} banned {msg.Message.MentionedUsers.First().Name} for reason ({msg.GetArg("reason")})");
+                    IEnumerable<Role> requiredRoles = bot.GetServer(261646233913917443).FindRoles("Moderation Team", false);
+                    foreach (Role role in requiredRoles)
+                    {
+                        if (msg.Message.MentionedUsers.First().HasRole(role)) { await msg.Channel.SendMessage($"You can't ban that person, {msg.Message.User.Mention}!"); return; }
+                          if (msg.Message.User.HasRole(role))
+                        {
+                            
+                            await bot.GetServer(261646233913917443).Ban(msg.Message.MentionedUsers.First());
+                            await bot.GetServer(261646233913917443).GetChannel(267022933547941890).SendMessage($"{msg.Message.User.Name} banned {msg.Message.MentionedUsers.First().Name} for reason ({msg.GetArg("reason")})");
+                            await msg.Message.Delete();
+                            return;
+                        }
+                        else
+                        {
+                            await msg.Channel.SendMessage($"You do not have permisson to run that command, {msg.Message.User.Mention}!");
+                            return;
+                        }
                     }
+                    await msg.Channel.SendMessage($"You do not have permisson to run that command, {msg.Message.User.Mention}!");
+
                 });
         }
     }
