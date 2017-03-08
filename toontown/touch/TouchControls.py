d09009b447b9d511d06e19666bb9dddc1c8467fb
Drew <drewgamesaw@gmail.com>
2017-03-08 15:19:43 -0500

    Update Touch Controls

--- toontown/touch/TouchControls.py
+++ toontown/touch/TouchControls.py
@@ -8,14 +8,30 @@ class TouchControls(DirectFrame, DirectObject):
     def __init__(self):
         DirectFrame.__init__(self, relief = None, sortOrder = 5000)
         gui = loader.loadModel('phase_3/models/gui/tt_m_gui_mat_mainGui.bam')
-        shuffleUp = gui.find('**/tt_t_gui_mat_shuffleUp')
-        shuffleDown = gui.find('**/tt_t_gui_mat_shuffleDown')
-        self.upButton = DirectButton(parent = self, relief = None, text_style = 3, image = (shuffleUp, shuffleDown, shuffleUp), image_scale = (.5, 0.7, 0.9), image1_scale = (0.5, 0.7, 0.9), image2_scale = (0.5, 0.7, 0.9), text_fg = (1, 1, 1, 1), text = "^", text_pos = (0, -0.02), text_scale = .07, scale = 1.2)
-        self.downButton = DirectButton(parent = self, relief = None, text_style = 3, image = (shuffleUp, shuffleDown, shuffleUp), image_scale = (.5, 0.7, 0.9), image1_scale = (0.5, 0.7, 0.9), image2_scale = (0.5, 0.7, 0.9), text_fg = (1, 1, 1, 1), text = "\/", text_pos = (0, -0.02), text_scale = .07, scale = 1.2)
-        self.leftButton = DirectButton(parent = self, relief = None, text_style = 3, image = (shuffleUp, shuffleDown, shuffleUp), image_scale = (.5, 0.7, 0.9), image1_scale = (0.5, 0.7, 0.9), image2_scale = (0.5, 0.7, 0.9), text_fg = (1, 1, 1, 1), text = "<", text_pos = (0, -0.02), text_scale = .07, scale = 1.2)
-        self.rightButton = DirectButton(parent = self, relief = None, text_style = 3, image = (shuffleUp, shuffleDown, shuffleUp), image_scale = (.5, 0.7, 0.9), image1_scale = (0.5, 0.7, 0.9), image2_scale = (0.5, 0.7, 0.9), text_fg = (1, 1, 1, 1), text = ">", text_pos = (0, -0.02), text_scale = .07, scale = 1.2)
-        self.jumpButton = DirectButton(parent = self, relief = None, text_style = 3, image = (shuffleUp, shuffleDown, shuffleUp), image_scale = (.5, 0.7, 0.9), image1_scale = (0.5, 0.7, 0.9), image2_scale = (0.5, 0.7, 0.9), text_fg = (1, 1, 1, 1), text = "o", text_pos = (0, -0.02), text_scale = .07, scale = 1.2)
-            
+        fwd = 'phase_3/maps/android/tui_move_up.png'
+        left = 'phase_3/maps/android/tui_move_l.png'
+        right = 'phase_3/maps/android/tui_move_r.png'
+        down = 'phase_3/maps/android/tui_move_dn.png'
+        jump = 'phase_3/maps/android/tui_move_jump.png'
+
+        self.upButton = DirectButton(parent = self, relief = None, text_style = 3, image = (fwd), image_scale = (.1), text_fg = (1, 1, 1, 1), text = "", scale = 1.2)
+        self.downButton = DirectButton(parent = self, relief = None, text_style = 3, image = (down), image_scale = (.1), text_fg = (1, 1, 1, 1), text = "", scale = 1.2)
+        self.leftButton = DirectButton(parent = self, relief = None, text_style = 3, image = (left), image_scale = (.1), text_fg = (1, 1, 1, 1), text = "", scale = 1.2)
+        self.rightButton = DirectButton(parent = self, relief = None, text_style = 3, image = (right), image_scale = (.1), text_fg = (1, 1, 1, 1), text = "", scale = 1.2)
+        self.jumpButton = DirectButton(parent = self, relief = None, text_style = 3, image = (jump), image_scale = (.1), text_fg = (1, 1, 1, 1), text = "", scale = 1.2)
+        
+        self.upButton.setTransparency(1)
+        self.downButton.setTransparency(1)
+        self.leftButton.setTransparency(1)
+        self.rightButton.setTransparency(1)
+        self.jumpButton.setTransparency(1)
+        
+        self.upButton.setColorScale(1, 1, 1, 0.75)
+        self.downButton.setColorScale(1, 1, 1, 0.75)
+        self.leftButton.setColorScale(1, 1, 1, 0.75)
+        self.rightButton.setColorScale(1, 1, 1, 0.75)
+        self.jumpButton.setColorScale(1, 1, 1, 0.75)
+        
         self.upButton.setPos(0, 0, .3)
         self.downButton.setPos(0, 0, -.3)
         self.leftButton.setPos(-.3, 0, 0)
@@ -96,7 +112,7 @@ class TouchControls(DirectFrame, DirectObject):
     def rightReleased(self, mouseEvent):
         messenger.send(base.MOVE_RIGHT + "-up")                    
                     
-                    
+                   
     def jumpClicked(self, mouseEvent):
         messenger.send(base.JUMP)
         
