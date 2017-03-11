ca8ba1932e6de8fa9d518a6020c1348256ac6e97
Drew <drewgamesaw@gmail.com>
2017-03-10 20:45:16 -0500

    rain mgr

--- toontown/environment/DistributedRainManager.py
+++ toontown/environment/DistributedRainManager.py
@@ -18,7 +18,7 @@ from toontown.battle.BattleProps import *
 
 class DistributedRainManager(DistributedWeatherMGR):
     notify = directNotify.newCategory('DistributedRainManager')
-    
+
     def __init__(self, cr):
         DistributedWeatherMGR.__init__(self, cr)
         self.cr = cr
@@ -37,10 +37,10 @@ class DistributedRainManager(DistributedWeatherMGR):
         self.bolt.setScale(5, 5, 90)
         self.bolt.setTransparency(1)
         self.bolt.setColorScale(1, 1, 0.2, 0.8)
-        
+
     def announceGenerate(self):
         DistributedWeatherMGR.announceGenerate(self)
-        
+
     def delete(self):
         render.setColorScale(1, 1, 1, 1)
         DistributedWeatherMGR.delete(self)
@@ -53,7 +53,7 @@ class DistributedRainManager(DistributedWeatherMGR):
             if self.rainSound:
                 self.rainSound.stop()
                 del self.rainSound
-            
+
             del self.rain
             del self.rainRender
 
@@ -65,12 +65,12 @@ class DistributedRainManager(DistributedWeatherMGR):
         self.rainRender.setDepthWrite(0)
         self.rainRender.setBin('fixed', 1)
         self.rain.start(camera, self.rainRender)
-        
+
         self.rainSound = base.loadSfx('phase_12/audio/sfx/CHQ_rain_ambient.ogg')
-        base.playSfx(self.rainSound, looping=1, volume=0.25)
-        
+        base.playSfx(self.rainSound, looping = 1, volume = 0.25)
+
         self.currentWeather = 0
-        
+
     def exitRain(self):
         pass
 
@@ -81,18 +81,18 @@ class DistributedRainManager(DistributedWeatherMGR):
         self.rainRender.setDepthWrite(0)
         self.rainRender.setBin('fixed', 1)
         self.rain.start(camera, self.rainRender)
-        
+
         # Winter time stuff
         self.wind1Sound = base.loadSfx('phase_8/audio/sfx/SZ_TB_wind_1.ogg')
         self.wind2Sound = base.loadSfx('phase_8/audio/sfx/SZ_TB_wind_2.ogg')
         self.wind3Sound = base.loadSfx('phase_8/audio/sfx/SZ_TB_wind_3.ogg')
         taskMgr.add(self.snowWindSoundTask, 'snowWind')
-        
+
         self.currentWeather = 0
-    
+
     def exitSnow(self):
         taskMgr.remove('snowWind')
-        
+
     def enterSunny(self, timestamp):
         self.hood.skyTransition('day')
         if self.rain:
@@ -100,16 +100,16 @@ class DistributedRainManager(DistributedWeatherMGR):
             if self.rainSound:
                 self.rainSound.stop()
                 del self.rainSound
-            
+
             del self.rain
             del self.rainRender
             taskMgr.remove('snowWind')
-        
+
         self.currentWeather = 1
 
     def exitSunny(self):
         pass
-        
+
     def enterThunderStorm(self, timestamp):
         self.hood.skyTransition('rain')
         render.setColorScale(0.8, 0.8, 0.8, 1)
@@ -120,18 +120,19 @@ class DistributedRainManager(DistributedWeatherMGR):
         self.rainRender.setBin('fixed', 1)
         self.rain.start(camera, self.rainRender)
         self.rainSound = base.loadSfx('phase_12/audio/sfx/CHQ_rain_ambient.ogg')
-        base.playSfx(self.rainSound, looping=1, volume=0.3)
-        
+        self.thunderSound = base.loadSfx('phase_6/audio/sfx/storm_lightning.ogg')
+        base.playSfx(self.rainSound, looping = 1, volume = 0.3)
+
         self.currentWeather = 0
-        
+
     def exitThunderStorm(self):
         pass
-        
+
     def snowWindSoundTask(self, task):
         now = globalClock.getFrameTime()
         if now < self.nextWindTime:
             return Task.cont
-        
+
         randNum = random.random()
         wind = int(randNum * 100) % 3 + 1
         if wind == 1:
@@ -140,20 +141,21 @@ class DistributedRainManager(DistributedWeatherMGR):
             base.playSfx(self.wind2Sound)
         elif wind == 3:
             base.playSfx(self.wind3Sound)
-        
+
         self.nextWindTime = now + randNum * 8.0
         return Task.cont
-        
+
     def spawnLightning(self, x, y):
         Sequence(
         Func(render.setColorScale, 1, 1, 1, 1),
         Wait(0.2),
         Func(render.setColorScale, 0.6, 0.6, 0.6, 1)).start()
+        self.thunderSound.play()
         self.bolt.reparentTo(render)
         self.bolt.setPos(x, y, -200)
         self.bolt.setH(random.randrange(-360, 360))
         taskMgr.doMethodLater(0.2, self.killLightning, 'killLightning')
-        
+
     def killLightning(self, task):
         if self.bolt:
             self.bolt.reparentTo(hidden)
