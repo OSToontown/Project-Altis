8e762aecfd589b61da9686805ec426253d3ced42
CheezedFish <watdou606@gmail.com>
2017-03-21 14:10:10 -0400

    Fix primary FO breakage, remnants of old source, re-enable SBFOs

--- toontown/cogdominium/SuitPlannerCogdoInteriorAI.py
+++ toontown/cogdominium/SuitPlannerCogdoInteriorAI.py
@@ -137,12 +137,14 @@ class SuitPlannerCogdoInteriorAI:
         return lvlList
 
     def __setupSuitInfo(self, suit, bldgTrack, suitLevel, suitType):
-        suitName, skeleton, flags = simbase.air.suitInvasionManager.getInvadingCog()
-        if suitName and self.respectInvasions:
-            suitType = SuitDNA.getSuitType(suitName)
-            bldgTrack = SuitDNA.getSuitDept(suitName)
-            suitLevel = min(max(suitLevel, suitType), suitType + 4)
-        
+        suitDeptIndex, suitTypeIndex, flags = simbase.air.suitInvasionManager.getInvadingCog()
+        if self.respectInvasions:
+            if suitDeptIndex is not None:
+                bldgTrack = SuitDNA.suitDepts[suitDeptIndex]
+            if suitTypeIndex is not None:
+                suitName = SuitDNA.getSuitName(suitDeptIndex, suitTypeIndex)
+                suitType = SuitDNA.getSuitType(suitName)
+                suitLevel = min(max(suitLevel, suitType), suitType + 4)
         dna = SuitDNA.SuitDNA()
         dna.newSuitRandom(suitType, bldgTrack)
         suit.dna = dna
