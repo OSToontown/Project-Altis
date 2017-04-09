15d8a3ec08eba427715c4e562d30472be96429e1
Drew <drewgamesaw@gmail.com>
2017-04-09 15:04:43 -0400

    1.7.14

--- config/release/dev.prc
+++ config/release/dev.prc
@@ -2,7 +2,7 @@
 model-path ../resources
 
 # Server:
-server-version TTPA-Alpha-1.7.13
+server-version TTPA-Alpha-1.7.14
 min-access-level 600
 accountdb-type local
 shard-low-pop 50
--- toontown/toonbase/ClientStartDist.py
+++ toontown/toonbase/ClientStartDist.py
@@ -34,7 +34,7 @@ key = 'g89a1hU0acBrlcru'
 # config = aes.decrypt(config, key, iv)
 
 config = """# Window settings:
-window-title Project Altis [ALPHA 1.7.13]
+window-title Project Altis [ALPHA 1.7.14]
 win-origin -2 -2
 icon-filename phase_3/etc/icon.ico
 cursor-filename phase_3/etc/toonmono.cur
@@ -201,7 +201,7 @@ want-old-fireworks #t
 want-live-updates #t
 
 # Server:
-server-version TTPA-Alpha-1.7.13
+server-version TTPA-Alpha-1.7.14
 shard-low-pop 50
 shard-mid-pop 80
 
