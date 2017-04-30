56998e0ade7317ec9bff10e181d3fad06e6eafa2
Drew <drewgamesaw@gmail.com>
2017-04-30 14:23:12 -0400

    Make dreamland darker and add some fog to it

--- toontown/safezone/DLPlayground.py
+++ toontown/safezone/DLPlayground.py
@@ -3,6 +3,16 @@ from toontown.safezone import Playground
 from toontown.toonbase import TTLocalizer
 
 class DLPlayground(Playground.Playground):
-	
+
+    def enter(self, requestStatus):
+        Playground.Playground.enter(self, requestStatus)
+        render.setColorScale(Vec4(.55, .55, .65, 1))
+        self.loader.hood.setDreamlandFog()
+
+    def exit(self):
+        Playground.Playground.exit(self)
+        render.setColorScale(Vec4(1, 1, 1, 1))
+        self.loader.hood.setNoFog()
+
     def showPaths(self):
         self.showPathPoints(CCharPaths.getPaths(TTLocalizer.Donald))
