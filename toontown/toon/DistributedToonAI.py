5c04697930b8e4076cb26af4e13ca2005c6d688c
Hunter <hunterray55@hotmail.com>
2017-05-04 08:59:34 -0400

    Lower required access level for inventory command

--- toontown/toon/DistributedToonAI.py
+++ toontown/toon/DistributedToonAI.py
@@ -5210,7 +5210,7 @@ def cogIndex(index):
     invoker.b_setCogIndex(index)
     return 'Set your Cog index to %d!' % index
 
-@magicWord(category=CATEGORY_PROGRAMMER, types=[str, int, int])
+@magicWord(category=CATEGORY_COMMUNITY_MANAGER, types=[str, int, int])
 def inventory(a, b=None, c=None):
     invoker = spellbook.getInvoker()
     inventory = invoker.inventory
