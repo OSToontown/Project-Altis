80de65bd5a321441c5e7c9da6a4c2aac7edf5ad7
Drew <drewgamesaw@gmail.com>
2017-05-07 19:59:35 -0400

    Update version number

--- toontown/toonbase/ClientStartDist.py
+++ toontown/toonbase/ClientStartDist.py
@@ -34,7 +34,7 @@ key = 'g89a1hU0acBrlcru'
 # config = aes.decrypt(config, key, iv)
 
 config = """# Window settings:
-window-title Project Altis [ALPHA 1.7.16]
+window-title Project Altis [ALPHA 1.7.17]
 win-origin -2 -2
 icon-filename phase_3/etc/icon.ico
 cursor-filename phase_3/etc/toonmono.cur
@@ -219,7 +219,7 @@ want-old-fireworks #t
 want-live-updates #t
 
 # Server:
-server-version TTPA-Alpha-1.7.16
+server-version TTPA-Alpha-1.7.17
 shard-low-pop 50
 shard-mid-pop 80
 
