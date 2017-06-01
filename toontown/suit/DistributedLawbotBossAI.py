8c7599ac5d4a15d2eb69b938211f469ff26e68a8
CheezedFish <watdou606@gmail.com>
2017-06-01 17:25:10 -0400

    Switch around summon weights to make them more useful

--- toontown/suit/DistributedLawbotBossAI.py
+++ toontown/suit/DistributedLawbotBossAI.py
@@ -643,7 +643,7 @@ class DistributedLawbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FSM
         self.b_setState('Reward')
         BattleExperienceAI.assignRewards(self.involvedToons, self.toonSkillPtsGained, self.suitsKilled, ToontownGlobals.dept2cogHQ(self.dept), self.helpfulToons)
         preferredDept = random.randrange(len(SuitDNA.suitDepts))
-        typeWeights = ['single'] * 70 + ['building'] * 27 + ['invasion'] * 3
+        typeWeights = ['single'] * 3 + ['building'] * 60 + ['invasion'] * 34
         preferredSummonType = random.choice(typeWeights)
         for toonId in self.involvedToons:
             toon = self.air.doId2do.get(toonId)
