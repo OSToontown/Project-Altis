edc5639a99c837201688a03cb2d08012bc61f7bc
CheezedFish <watdou606@gmail.com>
2017-07-30 17:06:07 -0400

    Implement a banId command

--- otp/ai/BanManagerAI.py
+++ otp/ai/BanManagerAI.py
@@ -143,3 +143,21 @@ def ban(reason):
     datagram.addString('You were banned by a moderator for the following reason: %s' % reason)
     simbase.air.send(datagram)
     return "Kicked and Banned %s from the game server!" % target.getName()
+	
+@magicWord(category=CATEGORY_MODERATOR, types=[int, str])
+def banId(id, reason):
+    """
+    Ban and Kick the short id from the game server.
+    """
+    target = simbase.air.doId2do.get(100000000+id)
+    if target == spellbook.getInvoker():
+        return "You can't ban yourself!"
+    simbase.air.banManager.ban(target.doId, reason)
+    datagram = PyDatagram()
+    datagram.addServerHeader(
+        target.GetPuppetConnectionChannel(target.doId),
+        simbase.air.ourChannel, CLIENTAGENT_EJECT)
+    datagram.addUint16(155)
+    datagram.addString('You were banned by a moderator for the following reason: %s' % reason)
+    simbase.air.send(datagram)
+    return "Kicked and Banned %s from the game server!" % target.getName()
