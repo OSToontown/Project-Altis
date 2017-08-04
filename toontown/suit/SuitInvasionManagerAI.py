5a90cf686dc0c0568b9e7cace9c3543250a3ad11
CheezedFish <watdou606@gmail.com>
2017-08-04 14:42:02 -0400

    slightly less

--- toontown/suit/SuitInvasionManagerAI.py
+++ toontown/suit/SuitInvasionManagerAI.py
@@ -77,7 +77,7 @@ class SuitInvasionManagerAI:
         if type == INVASION_TYPE_NORMAL:
             self.total = random.randint(1000, 5000)
         elif type == INVASION_TYPE_MEGA:
-            self.total = 1000000
+            self.total = 100000
         self.remaining = self.total
         self.type = type
 
