a6163709ba6fbea7e70d5e6d0484fc77070262f5
Cranky Supertoon <48419040+CrankySupertoon@users.noreply.github.com>
2020-03-18 23:24:42 -0400

    Update install-pip-packages.bat

--- tools/install-pip-packages.bat
+++ tools/install-pip-packages.bat
@@ -2,6 +2,9 @@
 cd ..
 title Project Altis Pip Packages Installer
 
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PYTHON_PATH=<PYTHON_PATH
+
 %PYTHON_PATH% -m pip install -r requirements.txt
 
 PAUSE
