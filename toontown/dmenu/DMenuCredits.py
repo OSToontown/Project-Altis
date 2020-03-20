f1eea14e58348dbbf4290da83234dbd3a1bba170
CrankySupertoon <crankysupertoon@gmail.com>
2020-03-20 19:49:29 -0400

    Rename Comrades Credits Entry

--- toontown/dmenu/DMenuCredits.py
+++ toontown/dmenu/DMenuCredits.py
@@ -27,7 +27,7 @@ class DMenuCredits:
 
 \1orangeText\1Open Source Toontown Servers\2
 Cranky Supetoon | Director
-ComradeNapCal | Friendly Neighborhood Rito
+ComradeNapCal | Notorious Leaker
 Rocket | Hackerman
 Leo | Major Oofenton
 ToonJoey | Resident Dinosaur Planet Guy
